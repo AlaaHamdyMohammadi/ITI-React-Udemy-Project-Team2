2168db123a0e7bb936aab3677bf4134b1f199c42
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-09-01 15:07:44 +0200

    HandleStructure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 /* eslint-disable no-unused-vars */
-import { useState } from 'react'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AppLayout from "./components/AppLayout";
+import NotFound from "./pages/NotFound";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [{ path: "*", element: <NotFound /> }],
+  },
+]);
 
 function App() {
-  
-
-  return 
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
-export default App
+export default App;
