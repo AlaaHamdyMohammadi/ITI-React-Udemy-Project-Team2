50d97ce05744e970c0c3f8967db42d2fc1399947
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-09-02 00:29:07 +0200

    ApplyTailwind

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,15 +1,17 @@
 function NotFound() {
-    return (
-      <div className="d-flex align-items-center justify-content-center vh-100">
+  return (
+    <>
+      <div className="flex h-screen items-center justify-center">
         <div className="text-center">
           <h1 className="display-1 fw-bold">404</h1>
-          <p className="fs-3">
-            <span className="text-danger">Opps!</span> Page not found.
+          <p className="text-3xl">
+            <span className="text-red-600">Opps!</span> Page not found.
           </p>
           <p className="lead">The page you’re looking for doesn’t exist.</p>
         </div>
       </div>
-    );
+    </>
+  );
 }
 
-export default NotFound
+export default NotFound;
