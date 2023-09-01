a727b5b633193d0dd69ca45e6a8db9a83fd62fa3
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-09-01 17:22:41 +0200

    HandleRouting

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 /* eslint-disable no-unused-vars */
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import AppLayout from "./components/AppLayout";
-import NotFound from "./pages/NotFound";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import AppLayout from './components/AppLayout';
+import NotFound from './pages/NotFound';
+import Login from './pages/Login';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <AppLayout />,
-    children: [{ path: "*", element: <NotFound /> }],
+    children: [
+      { path: '/login', element: <Login /> },
+      { path: '*', element: <NotFound /> },
+    ],
   },
 ]);
 
@@ -15,6 +19,8 @@ function App() {
   return (
     <>
       <RouterProvider router={router} />
+      <p className="text-center font-semibold text-red-600">Hello Tailwind</p>
+      <p className="text-primary">Hello Bootstrap</p>
     </>
   );
 }
