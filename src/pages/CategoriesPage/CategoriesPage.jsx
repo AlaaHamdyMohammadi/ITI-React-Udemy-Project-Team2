8a8ce1e4e121ecd55fd0c0bcb205f0201f2f2251
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-09-22 02:21:45 +0200

    Update

--- src/pages/CategoriesPage/CategoriesPage.jsx
+++ src/pages/CategoriesPage/CategoriesPage.jsx
@@ -3,12 +3,14 @@ import { useEffect, useState } from "react"
 import axiosInstance from "../../axiosConfig/instance";
 import { useParams } from "react-router-dom";
 import { Helmet } from "react-helmet";
-
+import { LiaGreaterThanSolid } from 'react-icons/Lia';
+import Categories from "../../components/userLogin/Categories";
+// import './CategoriesPage.css'
 
 function CategoriesPage() {
   const {_id} = useParams();
   const [category, setCategory] = useState({});
-  //console.log(_id)
+  const [subCategories, setSubCategories] = useState([]);
 
   useEffect(
     function () {
@@ -21,13 +23,23 @@ function CategoriesPage() {
     [_id],
   );
 
+  useEffect(function(){
+    axiosInstance.get(`/categories/${_id}/subCategories`).then(res => {
+      console.log(res.data.data.subCategories);
+      setSubCategories(res.data.data.subCategories);
+    });
+  }, [_id]);
+
   return (
     <>
       <Helmet>
         <title>{`${category.name} Online Courses | Udemy`}</title>
       </Helmet>
-      <div>
-        <h1>category: {category.name}</h1>
+      <div className="relative flex cursor-pointer justify-around pb-3 pt-3 shadow-md shadow-gray-300">
+        <span className="">
+          {category.name} <LiaGreaterThanSolid className="text-black inline text-1xl"/>
+        </span>
+        {subCategories.map((subCategory) => <div key={subCategory._id}>{subCategory.name}</div>)}
       </div>
     </>
   );
