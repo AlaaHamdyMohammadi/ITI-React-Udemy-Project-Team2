efa4a882032f3286c4f13111e22bc014bb5367a4
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-09-22 04:26:17 +0200

    Update

--- src/pages/CategoriesPage/CategoriesPage.jsx
+++ src/pages/CategoriesPage/CategoriesPage.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import { useEffect, useState } from 'react';
 import axiosInstance from '../../axiosConfig/instance';
 import { Link, NavLink, useParams } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import { LiaGreaterThanSolid } from 'react-icons/Lia';
+import { AiFillExclamationCircle } from 'react-icons/ai';
 
 function CategoriesPage() {
   const { _id } = useParams();
@@ -36,20 +38,9 @@ function CategoriesPage() {
       <Helmet>
         <title>{`${category.name} Online Courses | Udemy`}</title>
       </Helmet>
-      <div className="relative flex cursor-pointer justify-around pb-3 pt-3 shadow-md shadow-gray-300">
-        <NavLink className="font-bold text-gray-950 no-underline hover:text-violet-600">
-          {category.name}
-          <LiaGreaterThanSolid className="text-1xl inline text-gray-500" />
-        </NavLink>
-        {subCategories.map((subCategory) => (
-          <NavLink
-            className="text-gray-950 no-underline hover:text-violet-600"
-            key={subCategory._id}
-          >
-            {subCategory.name}
-          </NavLink>
-        ))}
-      </div>
+
+      <NavSubCategory category={category} subCategories={subCategories} />
+
       <div className="ml-4 mt-5">
         <h2 className="mb-5 font-bold">{category.name} Courses</h2>
         <h4 className="mb-3 font-bold">Courses to get you started</h4>
@@ -66,20 +57,58 @@ function CategoriesPage() {
       <div className="ml-4 mt-5">
         <h4 className="mb-3 font-bold">Featured course</h4>
       </div>
+
+      <PopularTopics subCategories={subCategories} />
+
       <div className="ml-4 mt-5">
-        <h4 className="mb-5 font-bold">Popular topics</h4>
-        <div className="flex flex-wrap justify-evenly">
-          {subCategories.map((subCategory) => (
-            <div className="" key={subCategory._id}>
-              <NavLink className=" border-1 p-3 font-bold text-gray-950 no-underline hover:bg-gray-100">
-                {subCategory.name}
-              </NavLink>
-            </div>
-          ))}
+        <h4 className="mb-3 font-bold">Popular Instructors</h4>
+      </div>
+      <div className="ml-4 mt-5">
+        <h4 className="mb-3 font-bold">All Data Science courses</h4>
+        <div className="border-1 p-3">
+          <AiFillExclamationCircle className="inline text-4xl" />
+          <span className="pl-3 text-md font-bold">
+            Not sure? All courses have a 30-day money-back guarantee
+          </span>
         </div>
       </div>
     </>
   );
+}
+
+function NavSubCategory({ category, subCategories }) {
+  return (
+    <div className="relative flex cursor-pointer justify-around pb-3 pt-3 shadow-md shadow-gray-300">
+      <NavLink className="font-bold text-gray-950 no-underline hover:text-violet-600">
+        {category.name}
+        <LiaGreaterThanSolid className="text-1xl inline text-gray-500" />
+      </NavLink>
+      {subCategories.map((subCategory) => (
+        <NavLink
+          className="text-gray-950 no-underline hover:text-violet-600"
+          key={subCategory._id}
+        >
+          {subCategory.name}
+        </NavLink>
+      ))}
+    </div>
+  );
+}
+
+function PopularTopics({subCategories}) {
+
+  return <div className="ml-4 mt-5">
+    <h4 className="mb-5 font-bold">Popular topics</h4>
+    <div className="flex flex-wrap justify-evenly">
+      {subCategories.map((subCategory) => (
+        <div className="" key={subCategory._id}>
+          <NavLink className=" border-1 p-3 font-bold text-gray-950 no-underline hover:bg-gray-100">
+            {subCategory.name}
+          </NavLink>
+        </div>
+      ))}
+    </div>
+  </div>
 }
 
 export default CategoriesPage;
